a3febe9cb80ad95657db8df0e242f21ae26c1d46
Cameron <cameronnakaba4@gmail.com>
2022-08-30 09:45:57 -0700

    sending data with fetch

--- index.js
+++ index.js
@@ -1,1 +1,64 @@
-// Add your code here
+
+
+
+function submitData(userName, userEmail) {
+  return fetch("http://localhost:3000/users",{
+      method:"POST",
+      headers: {
+          "Content-Type":"application/json",
+          "Accept":"application/json",
+      },
+      body:JSON.stringify({
+          "name":userName,
+          "email":userEmail
+      })
+  })
+
+  .then(response => response.json())
+
+  .then(data => renderId(data))
+
+
+  .catch((error)=> {
+      const span = document.createElement("span")
+      span.innerHTML = error.message;
+      document.body.append(span)
+  
+})
+}
+
+const renderId = data => {
+  const span = document.createElement("span")
+  span.innerHTML = data.id;
+  document.body.append(span)
+}
+
+
+
+
+
+
+
+/*
+const formData = {
+  dogName: "Byron",
+  dogBreed: "Poodle",
+};
+
+const configurationObject = {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json",
+  },
+  body: JSON.stringify(formData),
+};
+
+fetch("http://localhost:3000/dogs", configurationObject)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (object) {
+    console.log(object);
+  });
+*/
